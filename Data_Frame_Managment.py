37423e02d1c3d7befc713f964c667737591f45a4
BlueHalo9 <mialeimberg04@gmail.com>
2024-10-10 10:10:34 +0300

    data_frame

--- Data_Frame_Managment.py
+++ Data_Frame_Managment.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 
-data = pd.read_excel(r"Data.xlsx")
+data = pd.read_excel(r"Data (2).xlsx")
 
-df = pd.DataFrame(data, columns=['USERNAME','CARBON_FOOT_PRINT'])
+df = pd.DataFrame(data, columns=['USERNAME','CARBON_FOOT_PRINT',])
 
 df.set_index(['USERNAME', 'CARBON_FOOT_PRINT'])
 
-print(df.to_string(index=False))
+c = 'CARBON_FOOT_PRINT'
+
+b = df[c].sum()
+
+print(df)
