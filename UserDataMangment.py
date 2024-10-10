a9d7439d10d4ddc12ca741b116a9788d5b6c748b
AlonaHoz <alonahoz@gmail.com>
2024-10-10 11:10:25 +0300

    create_new_account

--- UserDataMangment.py
+++ UserDataMangment.py
@@ -2,14 +2,14 @@ from Main import user_data_list
 import consts
 import streamlit
 from pages import password_and_username
-from pages import  create_new_account
+from pages import create_new_account
+
 
 def create_user_data_dict(user_data_list):
     name = get_full_name(create_new_account.Full_Name)
-    id = get_id()
-    username = get_user_name(password_and_username.user_name)
-    password = get_password(password_and_username.password)
-    profile_img_file = get_profile_pic_file()
+    id = get_id(create_new_account.ID_Number)
+    username = get_user_name(create_new_account.UserName)
+    password = get_password(create_new_account.Password)
     elec_payment = get_electricity_account_payment()
     water_payment = get_water_account_payment()
     gas_payment = get_gas_account_payment()
@@ -39,15 +39,16 @@ def create_user_data_dict(user_data_list):
     user_data_list.append(user_data_dict)
     return user_data_list
 
+
 def add_percent_to_user_data(user_data_list, username, cfp_local_sum):
     for user in user_data_list:
         if user["USERNAME"] == username:
             username_elec_perc = calc_personal_user_percent_of_local_cfp_sum(cfp_local_sum,
                                                                              user["ELECTRICITY_CARBON_FOOT_PRINT"])
             username_water_perc = calc_personal_user_percent_of_local_cfp_sum(cfp_local_sum,
-                                                                             user["WATER_CARBON_FOOT_PRINT"])
+                                                                              user["WATER_CARBON_FOOT_PRINT"])
             username_gas_perc = calc_personal_user_percent_of_local_cfp_sum(cfp_local_sum,
-                                                                             user["GAS_CARBON_FOOT_PRINT"])
+                                                                            user["GAS_CARBON_FOOT_PRINT"])
             username_fuel_perc = calc_personal_user_percent_of_local_cfp_sum(cfp_local_sum,
                                                                              user["FUEL_CARBON_FOOT_PRINT"])
             user["USER_ELEC_PERCENT"] = username_elec_perc
@@ -55,6 +56,7 @@ def add_percent_to_user_data(user_data_list, username, cfp_local_sum):
             user["USER_GAS_PERCENT"] = username_gas_perc
             user["USER_FUEL_PERCENT"] = username_fuel_perc
             return user
+
 
 def get_user_data_dict(user_data_list, username):
     for user_dict in user_data_list:
@@ -70,15 +72,12 @@ def get_full_name(full_name):
     return full_name
 
 
-
-def get_id():
-    id_num = 0
-    while (not len(id_num) == 9) or (not id_num.is_integer()):
-        id_num = input("enter your id number: ")
-        if (not len(id_num) == 9) or (not id_num.is_integer()):
-            print("invalid input")
-    id_num = [id_num]
-    return id_num
+def get_id(ID_number):
+    while (not len(ID_number) == 9) or (not ID_number.is_integer()):
+        if (not len(ID_number) == 9) or (not ID_number.is_integer()):
+            streamlit.text("invalid input")
+    id_num = [ID_number]
+    return ID_number
 
 
 def get_user_name(user_name):
@@ -157,8 +156,6 @@ def calc_specific_cfp(source, emission):
 def calc_personal_user_percent_of_local_cfp_sum(cfp_local_sum, user_cfp_sum):
     user_percent = (user_cfp_sum * 100) / cfp_local_sum
     return [user_percent]
-
-
 
 
 def calc_specific_cfp(source, emission):
