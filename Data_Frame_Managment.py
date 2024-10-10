0bf818d055f73fb593889844268412109c40ce26
BlueHalo9 <mialeimberg04@gmail.com>
2024-10-10 14:51:34 +0300

    Data_frame

--- Data_Frame_Managment.py
+++ Data_Frame_Managment.py
@@ -1,38 +1,46 @@
+
 import pandas as pd
 
 
-exiting = 'Data (2).xlsx'
+exiting = 'foot_print_data_base.xlsx'
 data = pd.read_excel(exiting)
 
-df = pd.DataFrame(data, columns=['USERNAME','CARBON_FOOT_PRINT',])
+df = pd.DataFrame(data, columns=["USERNAME", "WATER_CARBON_FOOT_PRINT"])
 
-df.set_index(['USERNAME', 'CARBON_FOOT_PRINT'])
+df.set_index(['USERNAME', "WATER_CARBON_FOOT_PRINT"])
 
-cfp_local_sum = df['CARBON_FOOT_PRINT'].sum()
+cfp_local_sum = df["WATER_CARBON_FOOT_PRINT"].sum()
 
-print(df)
-
-exiting = 'Data (2).xlsx'
+exiting = 'foot_print_data_base.xlsx'
 
 name =input("name:")
 cfp = int(input("cfp:"))
 
-new_data = {'USERNAME': [name], 'CARBON_FOOT_PRINT': [cfp]}
+new_data = {"FULL_NAME": ["MIKEY"],
+        "ID_NUMBER": ["id"],
+        "USERNAME": ["SAMY"],
+        "PASSWORD": ["password"],
+        "ELECTRICAL_ACCOUNT_PAYMENT": ["elec_payment"],
+        "WATER_ACCOUNT_PAYMENT": ["water_payment"],
+        "GAS_ACCOUNT_PAYMENT": ["gas_payment"],
+        "FUEL_ACCOUNT_PAYMENT": ["fuel_payment"],
+        "PERSONAL_CFP_PERCENT": ["arbon_foot_print"],
+        "ELECTRICITY_CARBON_FOOT_PRINT": ["electricity_cfp"],
+        "WATER_CARBON_FOOT_PRINT": ["water_cfp"],
+        "GAS_CARBON_FOOT_PRINT": ["gas_cfp"],
+        "FUEL_CARBON_FOOT_PRINT": [86]}
 
-# Load existing workbook
-wb = load_workbook(existing_file)
 
-# Select the active sheet
-ws = wb.active
+df_new = pd.DataFrame(new_data)
 
-# Append new data
-for row in new_data:
-    ws.append(row)
+writer = pd.ExcelWriter('foot_print_data_base.xlsx', mode = 'a', if_sheet_exists= 'overlay')
 
-# Save the workbook
-wb.save(existing_file)
+df_new.to_excel(writer, index=False, header=False, startrow = len(data) +1)
+
+writer.close()
+
+data = pd.read_excel(exiting)
+
 print(df)
-
-
 
 
