8237d056f32ab01f387038687d73f80ae2b33ae1
BlueHalo9 <mialeimberg04@gmail.com>
2024-10-10 11:18:00 +0300

    Data_frame

--- Data_Frame_Managment.py
+++ Data_Frame_Managment.py
@@ -1,12 +1,38 @@
 import pandas as pd
 
 
-data = pd.read_excel(r"Data (2).xlsx")
+exiting = 'Data (2).xlsx'
+data = pd.read_excel(exiting)
 
 df = pd.DataFrame(data, columns=['USERNAME','CARBON_FOOT_PRINT',])
 
 df.set_index(['USERNAME', 'CARBON_FOOT_PRINT'])
 
-b = df['CARBON_FOOT_PRINT'].sum()
+cfp_local_sum = df['CARBON_FOOT_PRINT'].sum()
 
 print(df)
+
+exiting = 'Data (2).xlsx'
+
+name =input("name:")
+cfp = int(input("cfp:"))
+
+new_data = {'USERNAME': [name], 'CARBON_FOOT_PRINT': [cfp]}
+
+# Load existing workbook
+wb = load_workbook(existing_file)
+
+# Select the active sheet
+ws = wb.active
+
+# Append new data
+for row in new_data:
+    ws.append(row)
+
+# Save the workbook
+wb.save(existing_file)
+print(df)
+
+
+
+
