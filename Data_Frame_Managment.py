8586f4d5b915a81e573899b272797e010a577612
BlueHalo9 <mialeimberg04@gmail.com>
2024-10-10 15:30:28 +0300

    Data_frame

--- Data_Frame_Managment.py
+++ Data_Frame_Managment.py
@@ -1,46 +1,45 @@
 
 import pandas as pd
+from UserDataMangment import *
 
-
-exiting = 'foot_print_data_base.xlsx'
-data = pd.read_excel(exiting)
-
+the_data_base = 'foot_print_data_base.xlsx'
+data = pd.read_excel(the_data_base)
 df = pd.DataFrame(data, columns=["USERNAME", "WATER_CARBON_FOOT_PRINT"])
 
 df.set_index(['USERNAME', "WATER_CARBON_FOOT_PRINT"])
 
 cfp_local_sum = df["WATER_CARBON_FOOT_PRINT"].sum()
 
-exiting = 'foot_print_data_base.xlsx'
-
-name =input("name:")
-cfp = int(input("cfp:"))
-
-new_data = {"FULL_NAME": ["MIKEY"],
-        "ID_NUMBER": ["id"],
-        "USERNAME": ["SAMY"],
-        "PASSWORD": ["password"],
-        "ELECTRICAL_ACCOUNT_PAYMENT": ["elec_payment"],
-        "WATER_ACCOUNT_PAYMENT": ["water_payment"],
-        "GAS_ACCOUNT_PAYMENT": ["gas_payment"],
-        "FUEL_ACCOUNT_PAYMENT": ["fuel_payment"],
-        "PERSONAL_CFP_PERCENT": ["arbon_foot_print"],
-        "ELECTRICITY_CARBON_FOOT_PRINT": ["electricity_cfp"],
-        "WATER_CARBON_FOOT_PRINT": ["water_cfp"],
-        "GAS_CARBON_FOOT_PRINT": ["gas_cfp"],
-        "FUEL_CARBON_FOOT_PRINT": [86]}
-
-
-df_new = pd.DataFrame(new_data)
-
-writer = pd.ExcelWriter('foot_print_data_base.xlsx', mode = 'a', if_sheet_exists= 'overlay')
-
-df_new.to_excel(writer, index=False, header=False, startrow = len(data) +1)
-
-writer.close()
-
 data = pd.read_excel(exiting)
 
 print(df)
 
 
+name = the_list[2]
+
+foot_print_list = calc_list()
+
+def create_user_data_dict():
+    user_data_dict = {
+        "FULL_NAME": the_list[0],
+        "ID_NUMBER": the_list[1],
+        "USERNAME": the_list[2],
+        "PASSWORD": the_list[3],
+        "ELECTRICAL_ACCOUNT_PAYMENT": the_list[4],
+        "WATER_ACCOUNT_PAYMENT": the_list[5],
+        "GAS_ACCOUNT_PAYMENT": the_list[6],
+        "CAR_FUEL_PAYMENT": the_list[7],
+        "TOTAL_CARBON_FOOT_PRINT": foot_print_list[0],
+        "ELECTRICITY_CARBON_FOOT_PRINT": foot_print_list[1],
+        "WATER_CARBON_FOOT_PRINT": foot_print_list[2],
+        "GAS_CARBON_FOOT_PRINT": foot_print_list[3],
+        "FUEL_CARBON_FOOT_PRINT": foot_print_list[4]
+
+    }
+    return user_data_dict
+x = create_user_data_dict()
+
+# Append the new row to the DataFrame
+df = pd.concat([data, pd.DataFrame([x])], ignore_index=True)
+
+print(df)
